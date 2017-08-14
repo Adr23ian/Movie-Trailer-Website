a3b7d06296fb126bb2121549dbc5e1c1b29efb5f
Adrian Martinez <adr23ian@gmail.com>
2017-08-14 07:56:41 -0400

    Updated files for project without comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,37 @@
+import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story",
-                        "A STory of a Boy and His Toys that Come to Life",
-                        "")
+#Below are my favorite movie instances - all related to Math & Fight
+
+beautiful_mind = media.Movie("A Beautiful Mind",
+                             "From the heights of notoriety to the depths of depravity, John Nash Jr. a mathematical genius experienced it all.",
+                             "https://upload.wikimedia.org/wikipedia/en/b/b8/A_Beautiful_Mind_Poster.jpg",
+                             "https://www.youtube.com/watch?v=JV2PSWSyi0s")
+
+good_will_hunting = media.Movie("Good Will Hunting",
+                                "Will Hunting has a genius-level IQ but chooses to work as a janitor at MIT.",
+                                "https://upload.wikimedia.org/wikipedia/en/b/b8/Good_Will_Hunting_theatrical_poster.jpg",
+                                "https://www.youtube.com/watch?v=QSMvyuEeIyw")
+
+rain_man = media.Movie("Rain Man",
+                       "Raymond has savant syndrome and adheres to strict routines.",
+                       "https://upload.wikimedia.org/wikipedia/en/b/b2/Rain_Man_poster.jpg",
+                       "https://www.youtube.com/watch?v=KKC3W0awjm0")
+
+the_imitation_game = media.Movie("The Imitation Game",
+                                 "Based on the real life story of legendary cryptanalyst Alan Turing.",
+                                 "https://upload.wikimedia.org/wikipedia/en/5/5e/The_Imitation_Game_poster.jpg",
+                                 "https://www.youtube.com/watch?v=S5CjKEFb-sM")
+
+october_sky = media.Movie("October Sky",
+                          "From the coal mine to outer space",
+                          "https://upload.wikimedia.org/wikipedia/en/7/73/October_sky_poster.jpg",
+                          "https://www.youtube.com/watch?v=zxJQgYPXjN4")
+
+gladiator = media.Movie("Gladiator",
+                        "Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius.",
+                        "https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg",
+                        "https://www.youtube.com/watch?v=owK1qxDselE")
+
+movies = [beautiful_mind, good_will_hunting, rain_man, the_imitation_game, october_sky, gladiator]
+fresh_tomatoes.open_movies_page(movies)
